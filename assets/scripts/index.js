553151a8bc4a1fb0ad4b282655a129203a6956ff
StasNik <stas.nik12@gmail.com>
2023-09-14 10:29:25 +0300

    добавил мобильное меню

---
+++ assets/scripts/index.js
@@ -0,0 +1,6 @@
+function openOverlayMenu() {
+    document.querySelector(".menu__overlay").style.top = "0px";
+  }
+  function closeOverlayMenu() {
+    document.querySelector(".menu__overlay").style.top = "-100vh";
+  }

e45c3b8664d45ea295a60dbc48758601ac168b8d
KlinVid <stas.nik12@gmail.com>
2023-09-14 21:12:50 +0300

    скрипт языка

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,29 @@
+//Mobile menu
 function openOverlayMenu() {
     document.querySelector(".menu__overlay").style.top = "0px";
-  }
-  function closeOverlayMenu() {
-    document.querySelector(".menu__overlay").style.top = "-100vh";
-  }
+}
+
+function closeOverlayMenu() {
+  document.querySelector(".menu__overlay").style.top = "-100vh";
+}
+
+//Lang
+function changeLangRu() {
+  document.querySelector('.button__item.eng').style.display = 'none';
+  document.querySelector('.button__item.ru').style.display = 'flex';
+}
+
+function changeLangEng() {
+  document.querySelector('.button__item.ru').style.display = 'none';
+  document.querySelector('.button__item.eng').style.display = 'flex';
+}
+
+function changeLangRuMobile() {
+  document.querySelector('.button__item.eng').style.display = 'none';
+  document.querySelector('.button__item.ru').style.display = 'flex';
+}
+
+function changeLangEngMobile() {
+  document.querySelector('.button__item.ru').style.display = 'none';
+  document.querySelector('.button__item.eng').style.display = 'flex';
+}
